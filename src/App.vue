55a1ef052de230ca232381e3b408eaba0785de6a
bruno127 <bvasilj@gmail.com>
2022-08-27 13:54:16 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,26 @@
 <template>
-  <div id="app">
+  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? warm=true && 'warm' : warm=false && 'cold' ">
 <main>
       <div class="search-box">
         <input 
         type="text" 
         class="search-bar" 
         placeholder="Search..."
+        v-model = "query"
+        @keypress = "fetchWeather"
         />
       </div>
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location"> Mostar, BiH </div>
-          <div class="date"> 25.08.2022. </div>
+          <div class="location"> {{ weather.name }}, {{ weather.sys.country}}</div>
+          <div class="date">{{ dateBuilder() }}</div>
         </div>
 
         <div class="weather-box">
-          <div class="temp">32 °C</div>
-          <div class="weather"> Sunny </div>
+          <div class="temp">{{ Math.round(weather.main.temp) }} °C</div>
+          <div class ="feeling" v-if = "warm" > Hot in there 🥵 </div>
+          <div class ="feeling" v-else > It's pretty cold, what you think? 🥶 </div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
         </div>
       </div>
     </main>
@@ -26,6 +30,42 @@
 <script>
 export default {
   name: 'App',
+  data () {
+    return {
+      api_key: 'f58f58b6f41dd93092842f42a0750665',
+      url_base:'https://api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {},
+      warm: false,
+    }
+  },
+
+  methods: {
+    fetchWeather (e) {
+      if (e.key == "Enter") {
+      fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`).then(res => {
+        return res.json();
+      }).then(this.setResults);
+    }
+
+  },
+  setResults (results) {
+    this.weather = results;
+  },
+
+  dateBuilder () {
+    let d = new Date();
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+}
 }
 </script>
 
